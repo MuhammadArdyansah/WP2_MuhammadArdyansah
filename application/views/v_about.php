9652e6e3832f418a5b00634502c6f905bb96732c
MuhammadArdyansah <014rdyansah@gmail.com>
2022-04-04 20:49:24 +0700

    Mengumpulkan Tugas Pertemuan 3

---
+++ application/views/v_about.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+	<section>
+		<h1><?php echo $judul ?></h1>
+		<h4>Nama</h4>
+		<ul type="disc">
+			<li>Nama Depan : Muhammad</li>
+			<li>Nama Belakang : Ardyansah</li>
+		</ul>
+		<br>
+		<h4>Alamat</h4>
+		<ul type="none">
+			<li>Jalan Raya Lenteng Agung No.10A Jagakarsa</li>
+		</ul>
+
+		<h4>Tempat Lahir</h4>
+		<ul type="none">
+			<li>Jakarta</li>
+		</ul>
+
+		<h4>Olahraga Favorit</h4>
+		<ul type="square">
+			<li>Bulutangkis</li>
+			<li>Catur</li>
+		</ul>
+	</section>
+
+</body>
+</html>
---
+++ application/views/v_footer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+	<div>
+		<footer>
+			<a href="http://www.RentalBuku.com">RentalBuku</a>
+		</footer>
+	</div>
+</body>
+</html>
